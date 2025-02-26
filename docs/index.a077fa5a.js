628fa5b4bcb2a1e99ed90d15f612a140403d6af6
samuel-xiangpeng <samuel_xiangpeng@163.com>
2025-02-26 22:11:09 +0800

    feature:docs

---
+++ docs/index.a077fa5a.js
@@ -0,0 +1,6 @@
+document.addEventListener("copy",e=>{let{clipboardData:t}=e,n=document.getSelection();if(!t||!n)return;let a=`${n.toString()}
+  
+  License: CC-BY-SA-4.0
+  Thank you!
+  `;t.setData("text/plain",a),e.preventDefault()});
+//# sourceMappingURL=index.a077fa5a.js.map
